1f8ea2b95668c3fe25d340a8100d36baeaf0d3b5
“SURYAZ” <surya.prasad.v@gmail.com>
2023-10-27 16:42:39 +0530

    game search

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -6,18 +6,30 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import { BsSearch } from "react-icons/bs";
+import { useRef } from "react";
+interface Props {
+  onSearch: (searchText: string) => void;
+}
 
-const SearchInput = () => {
+const SearchInput = ({ onSearch }: Props) => {
+  const ref1 = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftAddon children={<BsSearch />} />
-      <Input
-        borderRadius={20}
-        placeholder={"Search games..."}
-        variant={"filled"}
-      />
-      <InputRightAddon children={<BsSearch />} />
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref1.current) onSearch(ref1.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftAddon children={<BsSearch />} />
+        <Input
+          ref={ref1}
+          borderRadius={20}
+          placeholder={"Search games..."}
+          variant={"filled"}
+        />
+      </InputGroup>
+    </form>
   );
 };
 
